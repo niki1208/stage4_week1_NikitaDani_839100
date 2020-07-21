c9af907bbcbf7bb50299da9db8fb5b540d172d3e
nutankadhane <nutankadhane@gmail.com>
2020-07-21 12:02:29 +0530

    week1 commit

---
+++ Week_1/Handson/HandsOn-2/HandsOn_1/src/main/java/com/cognizant/springlearn/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.SpringLearnApplication;
+
+@RestController
+public class HelloController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		LOGGER.info("START");
+		
+		LOGGER.info("END");
+		return "Hello World!!!";		
+	}
+}
